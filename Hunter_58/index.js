ba5cf56065908c3c051b956a06667473e9c8fe9f
Umesh-NIT <amarpuraumeshkumarpatel@gmail.com>
2025-05-19 14:44:35 +0530

    that is code for function

---
+++ Hunter_58/index.js
@@ -0,0 +1,33 @@
+function nice(name) {
+    console.log("Hey " + name + " you are good!");
+    console.log("Hey " + name + " you are good!");
+    console.log("Hey " + name + " you are good!");
+    console.log("Hey " + name + " you are working  good!");
+    console.log("Hey " + name + " Hey Umesh you are living good");
+    console.log("Hey " + name + " Hey Umesh you are living good");
+}
+nice("Shubham");
+nice("Umesh");
+function sum (a, b , c =3) {
+    console.log(a + b + c);
+    return a + b +c;
+   
+}
+result1 = sum(2, 6);
+result2 = sum(22, 3);
+result3 = sum(42, 3,1);
+console.log("The sum of these two numbers is: " + result1);
+console.log("The sum of these two numbers is: " + result2);
+console.log("The sum of these two numbers is: " + result3);
+// console.log("Hey Umesh you are good!");
+// console.log("Hey Umesh you are good!");
+// console.log("Hey Umesh you are good!");
+// console.log("Hey Umesh you are good!");
+// console.log("Hey Umesh you are working  good!");
+// console.log("Hey Umesh you are living good!");
+const function1 = (x)=>{
+    console.log("This is arrow function ",x);
+}
+function1(28);
+function1(23);
+function1(20);
