d84d7b58c0ca54135c7be56d9cd29c439c48df27
Umesh-NIT <amarpuraumeshkumarpatel@gmail.com>
2025-05-19 11:43:09 +0530

    that is loop code

---
+++ Hunter_57/index.js
@@ -0,0 +1,35 @@
+console.log("That is a turotrial on loops");
+let a =1;
+// console.log(a);
+// console.log(1 + a);
+// console.log(2 + a);
+// console.log(3 + a);
+// console.log(4 + a);
+
+// for (let i = 0;  i < 5; i++) {
+//     console.log(i + a);
+// }
+// let obj = {
+//     name: "John",
+//     Role: "Admin",
+//     city: "New York"
+// }
+
+// for (let key in obj) {
+    
+     
+//     console.log( key);
+// }
+
+// for (const c of "John") {
+//     console.log(c)
+// }
+let i =0;
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+// }
+do 
+{ console.log(i);
+    i++;
+} while (i < 6);
