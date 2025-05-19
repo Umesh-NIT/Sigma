e0a6f77868f7621daf9e97bcc9710c34cf95271e
Umesh-NIT <amarpuraumeshkumarpatel@gmail.com>
2025-05-19 11:12:03 +0530

    yes

---
+++ Hunter_56/index.js
@@ -0,0 +1,31 @@
+console.log("that's test js for hunter56");
+let age = 19  ;
+//let gradce = 12;
+//age += gradce;
+//console.log(gradce + age);
+//console.log(gradce - age);
+//console.log(gradce * age);
+//console.log(gradce /age);
+//console.log(gradce ** age);
+if (age == 18) {
+  console.log("you can drive");
+
+}
+else if (age == 0) {
+  console.log("are you kidding me");
+
+}
+else if (age > 18) {
+  console.log("are you again kidding me ?");}
+else {
+  console.log("you can not drive");
+}
+a = 6;
+b=8;
+let c=a>b  ? (a-b) : (b-a);
+//translates to:
+//if (a>b) {
+//  c = a-b;
+//} else {  let c = b-a;}
+console.log(c);
+
