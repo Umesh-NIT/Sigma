00c9a03309cdd3e82e50c453e05c7adc1bcd33bb
Umesh-NIT <amarpuraumeshkumarpatel@gmail.com>
2025-05-20 13:25:32 +0530

    solution of tutorial

---
+++ Hunter_62/index.js
@@ -0,0 +1,8 @@
+function nice(name) {
+  console.log("Crazy " + name + " Bros");
+  console.log("Amazing " + name + " Limited");
+  console.log("Fire " + name + " Hub");
+}
+nice("engine")
+nice("Foods")
+nice("Garments")
