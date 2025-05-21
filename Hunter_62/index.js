59007cd23d6d232f24d5c13f3dd3a6a618e3cbfe
Umesh-NIT <amarpuraumeshkumarpatel@gmail.com>
2025-05-21 10:35:02 +0530

    updated work

--- Hunter_62/index.js
+++ Hunter_62/index.js
@@ -1,8 +1,25 @@
-function nice(name) {
-  console.log("Crazy " + name + " Bros");
-  console.log("Amazing " + name + " Limited");
-  console.log("Fire " + name + " Hub");
-}
-nice("engine")
-nice("Foods")
-nice("Garments")
+// function nice(name) {
+//   console.log("Crazy " + name + " Bros");
+//   console.log("Amazing " + name + " Limited");
+//   console.log("Fire " + name + " Hub");
+// }
+// nice("engine")
+// nice("Foods")
+// nice("Garments")
+
+let obj = {
+  1: "crazy",   
+  2: "amazing",
+  3: "fire",}
+let obj1 = {
+  1: "engine",
+  2: "foods",
+  3: "garments",}
+let obj2 = {  
+  1:"bros", 
+  2: "limited",
+  3: "Hub",}
+  var random = Math.floor(Math.random() * 3) + 1;
+  console.log(random);  
+  console.log(obj[random], obj1[random], obj2[random]);
+  // console.log(obj[1], obj1[1], obj2[1]);
