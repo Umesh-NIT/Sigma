e09880dbfd57e2c2c9a5c1b3bc8433916c5c56f6
Umesh-NIT <amarpuraumeshkumarpatel@gmail.com>
2025-06-07 16:21:49 +0530

    Y

--- Hunter_70/script.js
+++ Hunter_70/script.js
@@ -85,19 +85,33 @@ console.log("Using DOM consept solving problem");
 //         box.style.backgroundColor = getRandomColor();
 //     });
 // });
-function getOctaColor() {
-    // Generate RGB using octal (0-7), scaled to 0-255
-    const r = Math.floor(Math.random() * 8) * 32; // 0–224
-    const g = Math.floor(Math.random() * 8) * 32;
-    const b = Math.floor(Math.random() * 8) * 32;
+// function getOctaColor() {
+//     // Generate RGB using octal (0-7), scaled to 0-255
+//     const r = Math.floor(Math.random() * 8) * 32; // 0–224
+//     const g = Math.floor(Math.random() * 8) * 32;
+//     const b = Math.floor(Math.random() * 8) * 32;
 
-    return `rgb(${r}, ${g}, ${b})`;
-}
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
 
-const boxes = document.querySelectorAll('.box');
+// const boxes = document.querySelectorAll('.box');
 
-boxes.forEach(box => {
-    box.style.backgroundColor = getOctaColor();
+// boxes.forEach(box => {
+//     box.style.backgroundColor = getOctaColor();
+// });
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}   
+document.querySelector(".rendomcolor").addEventListener("click", function () {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.backgroundColor = getRandomColor();
+    });
 });
-
-
---
+++ Hunter_73/index.js
@@ -0,0 +1,6 @@
+function createcard(title, cname, views , monthold, duratino , thumbnail){
+
+}
+createcard("Introductio to Backend | Hunter web development #23", "Huntercoder"
+    , "2M views", "2 month ago", "12:30", "thumbnail.jpg"
+)
